800e9fb5de1489c0d4f80ce280ae596d3c4e6e5a
default <rabira.motuma@stu.ocsb.ca>
2025-11-18 00:14:07 +0000

    added new main.ts file to find average of the inputed numbers.

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+ * Author: Rabira Motuma
+ * Version: 1.0.0
+ * Date 2025-11-14
+ * This program displays a multiplication table from 0 to 12, with the number 9.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//
+}
---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+* @author Rabira Motuma
+* @version 1.0.0
+* @date 2025-11-11
+* @fileoverview This program finds and displays the sidelengths of a cube with a volume of 1000 mm³.
+*/
+
+// INPUT - number data type number1
+let numbers : number = [56.9, 89.7, 90.2];
+
+// PROCESS
+// calculate answers
+// https://www.geeksforgeeks.org/javascript/how-to-compute-the-sum-and-average-of-elements-in-an-array-in-javascript/
+let sum : number = numbers[0] + numbers[1] + numbers[2];
+let average : number = sum / numbers.length
+
+// OUTPUT
+// display average in conclusion sentences
+console.log("The average of 56.9, 89.7, and 90.2 is " + average)
+
+console.log("\nDone.")
